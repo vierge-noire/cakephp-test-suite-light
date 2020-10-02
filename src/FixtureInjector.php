f5e49a6557c6ce634f03f74d664bc0e586809aa0
Juan Pablo Ramirez <juan-pablo.ramirez@postcon.de>
2020-10-02 18:20:33 +0200

    Fix

--- src/FixtureInjector.php
+++ src/FixtureInjector.php
@@ -78,7 +78,7 @@ class FixtureInjector extends \Cake\TestSuite\Fixture\FixtureInjector
      * @param float                   $time current time
      * @return void
      */
-    public function endTest(Test $test, float $time): void
+    public function endTest(Test $test, float $time)
     {
         // noop, see method description
     }
