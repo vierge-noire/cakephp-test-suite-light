6d5249f13f2683eb3f4d870a1f4c52a102d127b2
Juan Pablo Ramirez <juan-pablo.ramirez@postcon.de>
2020-10-07 15:57:37 +0200

    Namespaces updated in tests

--- tests/TestCase/Sniffer/TableSnifferTest.php
+++ tests/TestCase/Sniffer/TableSnifferTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * @since         1.0.0
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-namespace CakephpTestSuiteLight\Test\TestCase\TestSuite\Sniffer;
+namespace CakephpTestSuiteLight\Test\TestCase\Sniffer;
 
 
 use Cake\Database\Exception;
