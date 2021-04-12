a01cf37894fbd3f252e49f240cc53b3e7e49e871
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2021-04-12 02:46:09 +0200

    #32 Cleanup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -28,6 +28,7 @@ if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);
 }
 define('ROOT', dirname(__DIR__));
+define('TESTS', ROOT . DS . 'tests' . DS);
 define('APP_DIR', 'src');
 define('APP_PATH', ROOT . DS . 'TestApp' . DS);
 define('VENDOR_PATH', ROOT . DS . 'vendor' . DS);
@@ -46,7 +47,6 @@ define('TEST_APP', CORE_TESTS . 'TestApp' . DS);
 
 // Point app constants to the test app.
 define('APP', TEST_APP . 'src' . DS);
-define('TESTS', TEST_APP . 'tests' . DS);
 define('WWW_ROOT', TEST_APP . 'webroot' . DS);
 define('CONFIG', TEST_APP . 'config' . DS);
 
@@ -117,14 +117,14 @@ if (!getenv('DB_DRIVER')) {
 }
 $driver =  getenv('DB_DRIVER');
 
-if (!file_exists(ROOT . DS . '.env')) {
-    @copy(".env.$driver", ROOT . DS . '.env');
+if (!file_exists(TESTS . '.env')) {
+    @copy(TESTS . ".env.$driver", TESTS . '.env');
 }
 
 /**
  * Read .env file(s).
  */
-$loadEnv(ROOT . DS . '.env');
+$loadEnv(TESTS . '.env');
 
 // Re-read the driver
 $driver =  getenv('DB_DRIVER');
