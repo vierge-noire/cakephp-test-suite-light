75bace201073cf5212a504433dbece8485a07d20
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2021-09-20 01:45:21 +0200

    #47 Deprecate non triggered-based listener tests

--- run_tests.sh
+++ run_tests.sh
@@ -9,13 +9,3 @@ export DB_DRIVER=$DRIVER
 #### Tests with non temporary sniffers
 #######################
 ./vendor/bin/phpunit
-
-#### DEPRECATED #####
-#### Tests with temporary sniffers
-#### Skip MySQL
-#######################
-if [ $DRIVER != 'Mysql' ]; then
-  export SNIFFERS_IN_TEMP_MODE="true"
-  ./vendor/bin/phpunit
-fi
-
